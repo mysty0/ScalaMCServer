100132f08623bca11fa8f78661eeb5f030c9876b
Artem Goncharov <artemswork@mail.ru>
2018-01-11 14:11:17 +0800

    Added tests for chance and chunk section packet structure

--- src/test/scala/ChunkSectionTest.scala
+++ src/test/scala/ChunkSectionTest.scala
@@ -1,5 +1,7 @@
+import akka.util.ByteString
 import com.scalamc.models.world.Block
 import com.scalamc.models.world.chunk.ChunkSection
+import com.scalamc.utils.{ByteBuffer, ByteStack}
 import org.scalatest.{FunSuite, GivenWhenThen, Matchers}
 
 class ChunkSectionTest extends FunSuite with GivenWhenThen with Matchers {
@@ -7,6 +9,35 @@ class ChunkSectionTest extends FunSuite with GivenWhenThen with Matchers {
     var chunkSection = new ChunkSection()
     chunkSection.setBlock(0, 0, 0, Block(4, 0))
     assert(chunkSection.getBlock(0,0,0).id == 4 && chunkSection.getBlock(0,1,0).id==0)
+  }
+
+  test("write and read type1"){
+    val bitsPerBlock = 13
+    val blockId = 4
+    val lastBlockId = 7
+    val metadata: Byte = 0
+
+    var chunkSection = new ChunkSection()
+    chunkSection.setBlock(0, 0, 0, Block(blockId, metadata))
+    chunkSection.setBlock(15, 15, 15, Block(lastBlockId, metadata))
+    var buf: ByteBuffer = new ByteBuffer()
+    chunkSection.writeToBuff(buf, true)
+    var stack = new ByteStack(buf.toArray)
+    assert(stack.pop() == bitsPerBlock)
+    assert(stack.pop() == 0)
+    assert(stack.popVarInt() == 4096 * bitsPerBlock / 64)
+
+    assert(stack.popLong() == ((blockId << 4) | metadata))
+    assert(stack.popLong() == 0)
+    stack.popWith((4096 * bitsPerBlock / 64 - 3) * 8)
+    val lastBlock = stack.popLong()
+    println(lastBlock)
+    assert((lastBlock >> (64 - 13)) == ((lastBlockId << 4) | metadata))
+
+    assert(stack.length == 4096)
+    assert(stack.pop() == 0xFF.toByte)
+    stack.popWith(4096 - 2)
+    assert(stack.pop() == 0xFF.toByte)
   }
 
 }
--- src/test/scala/ChunkTest.scala
+++ src/test/scala/ChunkTest.scala
@@ -1,6 +1,9 @@
+import akka.util.ByteString
 import com.scalamc.models.world.Block
-import com.scalamc.models.world.chunk.Chunk
+import com.scalamc.models.world.chunk.{Chunk, ChunkSection}
 import org.scalatest.{FunSuite, GivenWhenThen, Matchers}
+import com.scalamc.packets.Packet._
+import com.scalamc.utils.{ByteBuffer, ByteStack}
 
 class ChunkTest extends FunSuite with GivenWhenThen with Matchers {
   test("set and get type"){
@@ -9,4 +12,33 @@ class ChunkTest extends FunSuite with GivenWhenThen with Matchers {
     println(chunk.getSection(0).getBlock(0,0,0))
     assert(chunk.getBlock(0,0,0).id == 10)
   }
+
+  test("test chunk packet"){
+    // given
+    val x = 10
+    val z = 20
+    val groundUpContinues = true
+    val sectionNum: Byte = 0
+    val blockId = 10
+
+    // when
+    var chunk = new Chunk(x, z)
+    chunk.setBlock(0, sectionNum * 16, 0, Block(blockId, 0))
+    var buf: ByteString = chunk.toPacket(true, groundUpContinues)
+    var stack = new ByteStack(buf.toArray)
+    assert(stack.popVarInt() == stack.length)
+    assert(stack.popVarInt() == 0x20)
+    assert(stack.popInt() == x)
+    assert(stack.popInt() == z)
+    assert(stack.pop() == (if (groundUpContinues) 1 else 0))
+    assert(stack.popVarInt() == (1 << sectionNum))
+    assert(stack.popVarInt() == stack.length - 1)
+
+    var chunkSection = new ChunkSection()
+    chunkSection.setBlock(0, 0, 0, Block(blockId, 0))
+    var bufSection: ByteBuffer = new ByteBuffer()
+    chunkSection.writeToBuff(bufSection, true)
+    assert(stack.popWith(stack.length - 1).toString() == bufSection.toString())
+  }
+
 }
